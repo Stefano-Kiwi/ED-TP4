dbcaf187f6dd04a15cf207a413e6a0f9feb035e2
Stefano-Kiwi <stefanobudon@gmail.com>
2022-05-10 14:03:26 -0300

    Commit inicial

---
+++ src/main/java/com/mycompany/trabajopractico4/TrabajoPractico4.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.trabajopractico4;
+
+/**
+ *
+ * @author stefa
+ */
+public class TrabajoPractico4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
