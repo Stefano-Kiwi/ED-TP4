ea388047a6e62dfb3a4d16a05d8ac6035a76af7a
Stefano-Kiwi <stefanobudon@gmail.com>
2022-05-13 14:57:32 -0300

    Agrego repo al pom

---
+++ src/main/java/Ejercicio1/ArbolBinarioExtInterfaz.java
@@ -0,0 +1,20 @@
+package Ejercicio1;
+
+/**
+ *
+ * @author stefa
+ */
+public interface ArbolBinarioExtInterfaz <T>{
+    
+    /** Indica la cantidad de elementos almacenados en la estructura. */
+    public int size();
+    /** Indica si dos nodos son hermanos*/
+    public boolean hermanos(NodoArbolBinario<T> nodo1, NodoArbolBinario<T>
+    nodo2);
+    /** Devuelve todos los nodos hoja de la estructura*/
+    public ListaEnlazada<T> hojas();
+    /** Devuelve todos los nodos que no son raíz ni hoja*/
+    public ListaEnlazada<T> internos();
+    /** Devuelve todos los nodos que pertenecen al nivel pasado por parámetro.*/
+    public ListaEnlazada<T> nivel(int nivel);
+}
