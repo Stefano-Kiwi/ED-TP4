ecb8a56cc9cfae46a1a2869ee8b7e4b7a86b7018
Stefano-Kiwi <stefanobudon@gmail.com>
2022-05-29 17:16:13 -0300

    Terminado cliente para Cola usando Lista

--- src/main/java/Ejercicio3/ClienteColaPrioridad.java
+++ src/main/java/Ejercicio3/ClienteColaPrioridad.java
@@ -1,21 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Ejercicio3;
 
-/**
- *
- * @author David Zarcó
- */
+package Ejercicio3;
+import Ejercicio4.*;
+
+
 public class ClienteColaPrioridad {
     public static void main(String[] args) {
-        ColaPrioridad cola = new ColaPrioridad();
-        System.out.println("Esta vacia? "+ cola.isEmpty());
-        cola.insert(10, "arbol");
-        System.out.println("Esta vacia? "+ cola.isEmpty());
+        // ESTO PARA LA COLA PRIORIDAD HECHA CON ARRAY
+//        
+//        ColaPrioridad cola = new ColaPrioridad();
+//        System.out.println("Esta vacia? "+ cola.isEmpty());
+//        cola.insert(10, "arbol");
+//        System.out.println("Esta vacia? "+ cola.isEmpty());
+//        
         
         
+        
+        
+        
+        
+    
+        
+        
+        // ESTO PARA LA COLA PRIORIDAD HECHA CON LISTA NO ORDENADA
+        
+     ColaPrioridadConLista colaLista = new ColaPrioridadConLista();
+     colaLista.insert(1, "pikachu");
+     colaLista.insert(1, "pichu");
+     colaLista.insert(10, "voltorb");
+     colaLista.insert(10, "arcanine");
+     colaLista.insert(20, "eevee");
+     colaLista.insert(20, "flareon");
+     colaLista.insert(20, "vaporeon");
+     colaLista.insert(20, "sylveon");
+     colaLista.insert(18, "nidorina");
+     colaLista.insert(2, "charmander");
+     colaLista.insert(2, "charmilion");
+     colaLista.insert(2, "charizard");
+     colaLista.insert(3, "bulbasaur");
+     colaLista.insert(4, "wartortle");
+     colaLista.insert(4, "blastoise");
+       
+        System.out.println("ColaLista: "+colaLista); // SE IMPRIMEN NUMEROS DEBIDO AL METODO .get() de ListaEnlazadaNoOrdenada
+        System.out.println("Cantidad de elementos en colaLista:"+colaLista.size());
+        System.out.println("Elemento con mayor prioridad: "+colaLista.max());
+        colaLista.deleteMax();
+        System.out.println("Lista al eliminar el elemento con mayor prioridad(quito eevee): "+colaLista);
         
     }
     
